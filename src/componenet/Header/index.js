51d72207ed16b21fc724e96c08105b1bfabccdee
AK <arshit.n.p.8@gmail.com>
2022-01-20 15:51:36 +0530

    uplode first

---
+++ src/componenet/Header/index.js
@@ -0,0 +1,10 @@
+import react from "react";
+import './index.css';
+
+export default function index() {
+  return <div>
+    <div className="navbar">
+      <h1 className="Todos">Todos</h1>
+    </div>
+  </div>;
+}
