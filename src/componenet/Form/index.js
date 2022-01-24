86414287620265b9d474a1bafa96e77bab93cf86
koshiya arshit v <97168905+arshitkoshiya@users.noreply.github.com>
2022-01-24 16:36:17 +0530

    Update index.js

--- src/componenet/Form/index.js
+++ src/componenet/Form/index.js
@@ -1,28 +1,69 @@
 import React from "react";
 import "./index.css";
+import List from "../Todos/index.json";
+import { useState } from "react/cjs/react.development";
 
-export default function index() {
+export default function Form(props) {
+  function update() {
+    List.push({
+      Title: a,
+      Description: b,
+      Status: c,
+    });
+    /* alert(JSON.stringify(List)); */
+    props.form();
+  }
+  const [a, seta] = useState();
+  const [b, setb] = useState();
+  const [c, setc] = useState();
+  function logvalue(e) {
+    console.log("hello")
+    if(e.target.name === 'title'){
+      seta(e.target.value)
+    }
+    else if(e.target.name === 'Description'){
+      setb(e.target.value)
+    }
+    else{
+      setc(e.target.value)
+
+    }
+  }
   return (
     <>
       <div className="form">
-        <form action="" method="post" >
+        <form action="" method="post">
           <label htmlFor="">Title </label>
-          <input type="text" name="text" id="titlebox" placeholder="enter title" /><br />
+          <input
+            type="text"
+            name="title"
+            id="titlebox"
+            placeholder="enter title"
+            onChange={(e) => logvalue(e)}
+          />
+          <br />
           <label htmlFor="">Description </label>
           <textarea
-            name=""
+            name="Description"
             id="textarea"
             cols="20"
             rows="5"
             placeholder="Description"
-          ></textarea><br />
+            onChange={(e) => logvalue(e)}
+          ></textarea>
+          <br />
           <label htmlFor="">Status</label>
-          <select name="select" id="">
-            <option value="">Todo</option>
-            <option value="">InProgress</option>
-            <option value="">Done </option>
-          </select><br />
-          <input type="button" value="Add" id="btn1" />
+          <select name="select" id="select" onChange={(e) => logvalue(e)}>
+            <option value="" selected disabled>--select--</option>
+            <option value="Todo">Todo</option>
+            <option value="InProgress">InProgress</option>
+            <option value="Done">Done </option>
+          </select>
+          <br />
+          <br />
+          <div className="btn1">
+            <input type="button" value="Add" id="btn1" onClick={update} />
+          </div>
         </form>
       </div>
     </>
