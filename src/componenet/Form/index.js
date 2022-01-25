0c0e821219cedbb17bc3760cbb40e394f726243f
AK <arshit.n.p.8@gmail.com>
2022-01-25 10:48:57 +0530

    Done

--- src/componenet/Form/index.js
+++ src/componenet/Form/index.js
@@ -1,28 +1,99 @@
 import React from "react";
 import "./index.css";
+import List from "../Todos/index.json";
+import { useState, useEffect } from "react/cjs/react.development";
 
-export default function index() {
+export default function Form(props) {
+  function update() {
+    List.push({
+      Id:List[List.length - 1]['Id'] + 1,
+      Title: a,
+      Description: b,
+      Status: c,
+    });
+    /* alert(JSON.stringify(List)); */
+    props.form();
+  }
+  const [a, seta] = useState();
+  const [b, setb] = useState();
+  const [c, setc] = useState();
+  function logvalue(e) {
+    if (e.target.name === "title") {
+      seta(e.target.value);
+    } else if (e.target.name === "Description") {
+      setb(e.target.value);
+    } else {
+      setc(e.target.value);
+    }
+  }
+
+  const create = () => {
+    List.filter((xyz) => xyz.Id === props.editid.Id).map((mno) => {
+      mno.Title = a;
+      mno.Description = b;
+      mno.Status = c;
+    });
+
+    props.form();
+    props.setid(null);
+  };
+
+  useEffect(() => {
+    if (props.editid != null) {
+      seta(props.editid.Title);
+      setb(props.editid.Description);
+      setc(props.editid.Status);
+    }
+  }, []);
+
   return (
     <>
       <div className="form">
-        <form action="" method="post" >
+        <form action="" method="post">
           <label htmlFor="">Title </label>
-          <input type="text" name="text" id="titlebox" placeholder="enter title" /><br />
+          <input
+            type="text"
+            name="title"
+            id="titlebox"
+            value={a}
+            placeholder="enter title"
+            onChange={(e) => logvalue(e)}
+          />
+          <br />
           <label htmlFor="">Description </label>
           <textarea
-            name=""
+            name="Description"
             id="textarea"
             cols="20"
             rows="5"
+            value={b}
             placeholder="Description"
-          ></textarea><br />
+            onChange={(e) => logvalue(e)}
+          ></textarea>
+          <br />
           <label htmlFor="">Status</label>
-          <select name="select" id="">
-            <option value="">Todo</option>
-            <option value="">InProgress</option>
-            <option value="">Done </option>
-          </select><br />
-          <input type="button" value="Add" id="btn1" />
+          <select
+            name="select"
+            id="select"
+            value={c}
+            onChange={(e) => logvalue(e)}
+          >
+            <option value="" selected disabled>
+              --select--
+            </option>
+            <option value="Todo">Todo</option>
+            <option value="InProgress">InProgress</option>
+            <option value="Done">Done </option>
+          </select>
+          <br />
+          <br />
+          <div className="btn1">
+            {props.editid != null ? (
+              <input type="button" value="Edit" id="btn1" onClick={create} />
+            ) : (
+              <input type="button" value="Add" id="btn1" onClick={update} />
+            )}
+          </div>
         </form>
       </div>
     </>
--- src/componenet/Todos/index.js
+++ src/componenet/Todos/index.js
@@ -4,52 +4,82 @@ import List from "./index.json";
 import Form from "../Form/index.js";
 
 export default function Index() {
-  const Todo = List.filter((abc) => abc.Status === "Todo").map((todolist) => (
-    <div>{todolist.Title} : {todolist.Description}</div>
+  const Todo = List.filter((abc) => abc.Status === "Todo").map((todolist,Index) => (
+    <div key={Index}>
+      {todolist.Title} : {todolist.Description}{" "}
+      <input type="button" value="Edit" id="btn" onClick={(e) => edit(todolist)} />
+    </div>
   ));
   const InProgress = List.filter((abc) => abc.Status === "InProgress").map(
-    (todolist) => <div>{todolist.Title} : {todolist.Description}</div>
+    (todolist, Index) => (
+      <div key={Index}>
+        {todolist.Title} : {todolist.Description}{" "}
+        <input type="button" value="Edit" id="btn" onClick={(e) => edit(todolist)}/>
+      </div>
+    )
   );
-  const Done = List.filter((abc) => abc.Status === "Done").map((todolist) => (
-    <div>{todolist.Title} : {todolist.Description}</div>
+  const Done = List.filter((abc) => abc.Status === "Done").map((todolist, Index) => (
+    <div key={Index}>
+      {todolist.Title} : {todolist.Description}{" "}
+      <input type="button" value="Remove" id="btn" onClick={(e) => remove(todolist.Id)}  />
+    </div>
   ));
   const [createTodo, setcreateTodo] = useState(true);
+  const [id, setid] = useState(null);
 
-  function createtodo( ) {
+  function createtodo() {
+    setcreateTodo(!createTodo);
+  }
 
-	setcreateTodo(!createTodo)
+  const edit = (editid) =>{
+    setid(editid);
+  createtodo();
+  }
 
+const [removeffect,setremoveffect] = useState(false);
+
+  const remove = (dremove) => {
+
+const index = List.findIndex((rem) => rem.Id === dremove)
+List.splice(index, 1);
+ setremoveffect(!removeffect);
   }
 
   return (
     <>
-		{createTodo ?<div> <div className="grid-container">
-        <div className="class1">
-          Todos
-          <hr />
-          {Todo}
+      {createTodo ? (
+        <div>
+          {" "}
+          <div className="grid-container">
+            <div className="class1">
+              Todos
+              <hr />
+              {Todo}
+            </div>
+            <div className="class2">
+              InProcess
+              <hr />
+              {InProgress}
+            </div>
+            <div className="class3">
+              Done
+              <hr />
+              {Done}
+            </div>
+          </div>
+          <br />
+          <div className="btn">
+            <input
+              type="button"
+              value="Create Todos"
+              id="btn"
+              onClick={createtodo}
+            />
+          </div>
         </div>
-        <div className="class2">
-          InProcess
-          <hr />
-          {InProgress}
-        </div>
-        <div className="class3">
-          Done
-          <hr />
-          {Done}
-        </div>
-      </div>
-      <br />
-      <div className="btn">
-        <input
-          type="button"
-          value="Create Todos"
-          id="btn"
-          onClick={createtodo}
-        />
-      </div></div>: <Form  form = {createtodo}	/> }
-      
+      ) : (
+        <Form form={createtodo} editid={id} setid={setid}/>
+      )}
     </>
   );
 }
